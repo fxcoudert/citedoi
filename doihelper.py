ccf3c2ac4eed2ee90dc21aed431fbaa6ebd8604b
FX <fxcoudert@gmail.com>
2015-07-04 08:39:07 +0200

    Initial import of the sources

---
+++ doihelper.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys, os, requests
+
+def doi2bib(doi):
+  """
+  Return a bibTeX string of metadata for a given DOI.
+  """
+ 
+  url = "http://dx.doi.org/" + doi
+  headers = {"accept": "application/x-bibtex"}
+  r = requests.get(url, headers = headers)
+  return r.text
+
+def replacebibkey(bib, key):
+  y, z = bib.split(",", 1)
+  x, y = y.split("{", 1)
+  return x + "{" + key + "," + z
+
+def iskeyalreadypresent(key, bibfile):
+  try:
+    f = open(bibfile, "r")
+  except:
+    return False
+
+  for line in f:
+    if line[0] == '@' and key in line:
+      return True
+
+  return False
+
+def main():
+  bibfile = sys.argv[1] + ".bib"
+  doi = sys.argv[2]
+  key = sys.argv[3]
+
+  if iskeyalreadypresent(key, bibfile):
+    return
+
+  try:
+    bib = doi2bib(doi)
+  except:
+    return
+
+  bib = replacebibkey(bib, key)
+  with open(bibfile, "a") as f:
+    f.write(bib + "\n")
+
+if __name__ == '__main__':
+  main()
